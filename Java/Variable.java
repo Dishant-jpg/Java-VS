4f19bc35607ace0d0f151acdb227ea0fb97d9b74
Dishant Gupta <guptadishant90@gmail.com>
2022-12-18 15:16:24 +0530

    Java

---
+++ Java/Variable.java
@@ -0,0 +1,16 @@
+class variable{
+    public static void main(String args[]) {
+
+        //variable
+        int a = 50;
+        int b = 25;
+        var sum = a + b;
+        System.out.print(sum);
+        int diff = b - a;
+        System.out.print(diff);
+        int mul = a * b;
+        System.out.print(mul);
+
+
+    }
+}
