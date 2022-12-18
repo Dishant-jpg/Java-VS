152cf4366d279c32a80f9ee4055b4cd33ae5479f
Dishant Gupta <guptadishant90@gmail.com>
2022-12-18 18:48:20 +0530

    Java

--- Java/input.java
+++ Java/input.java
@@ -2,6 +2,13 @@ import java.util.*;
 class input{
     public static void main(String args[]) {
       //Input
-       Scanner sc = new Scanner(System.in); 
+       Scanner sc = new Scanner(System.in);
+       //String name = sc.next(); 
+       //System.out.println(name);
+       int a = sc.nextInt();
+       int b = sc.nextInt();
+       int sum = a + b;
+       System.out.println(sum);
+
     }
 }
